63fa43deab5e986c1bafd129dda77fbff43a874b
Vishal Thakur <vischalthakur@gmail.com>
2025-04-22 22:31:15 +1000

    Update testing-target.cpp

--- misc/testing-target.cpp
+++ misc/testing-target.cpp
@@ -1,3 +1,4 @@
+//use this file to build an executable for testing the injection on. This bin should keep running for a minute which should give you enough time to launch the warhead. 
 #include <windows.h>
 int main() {
     Sleep(60000);  // Sleep 1 minute so you can inject into it
